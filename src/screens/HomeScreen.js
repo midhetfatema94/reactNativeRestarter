785cff44b26cf11b85b7d95df1ad2457c70b2b48
Midhet Sulemani <midhetfatema94@gmail.com>
2019-12-01 17:40:01 +0300

    added buttons

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hello World</Text>;
-};
+  return (
+    <View>
+      <Text style={styles.text}>Hello World</Text>
+      <Button 
+        title="Components Demo" 
+        onPress={() => console.log("button pressed")}
+      />
+      <TouchableOpacity onPress={() => console.log("list button pressed")}>
+        <Text>List Demo</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
+    fontSize: 30,
+    textAlign: "center"
   }
 });
 
